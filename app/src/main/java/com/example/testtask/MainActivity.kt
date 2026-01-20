019569e8b519f7c955b4284452be57e700896a09
Dvoretskyi Oleksandr <91936573+nypzlst@users.noreply.github.com>
2026-01-20 22:58:37 +0200

    Add Task List
    Need small fix

--- app/src/main/java/com/example/testtask/MainActivity.kt
+++ app/src/main/java/com/example/testtask/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
@@ -24,25 +25,50 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.testtask.ui.theme.TestTaskTheme
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.input.TextFieldValue
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            TestTaskTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Row(
-                        modifier = Modifier.statusBarsPadding().fillMaxSize().padding(16.dp)
-                    ) {
-                        TextTODOField (
-                            modifier = Modifier.weight(1f)
-                        )
-                        FilledButton(onClick = {
-                            println("Кнопка нажата!")
-                        })
-                    }
 
+        setContent {
+            val tasks = remember { mutableStateListOf<Task>() }
+            var todoText by remember { mutableStateOf(TextFieldValue("")) }
+
+            TestTaskTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->(
+                        Column(
+                            modifier = Modifier.fillMaxSize().padding(innerPadding)
+                        ) {
+                            Row(
+                                modifier = Modifier.statusBarsPadding().fillMaxWidth().padding(16.dp)
+                            ) {
+                                TextTODOField (
+                                    modifier = Modifier.weight(1f),
+                                    value = todoText,
+                                    onValueChange = { todoText = it },
+                                )
+
+                                FilledButton(onClick = {
+                                    if(todoText.text.isNotBlank()){
+                                        tasks.add(Task(id = tasks.size, title = todoText.text))
+                                        todoText = TextFieldValue("")
+                                    }
+
+                                })
+
+                            }
+                            TaskList(tasks = tasks)
+                        }
+                        )
                 }
             }
         }
@@ -61,10 +87,35 @@ fun FilledButton(onClick: () -> Unit, modifier: Modifier = Modifier){
 }
 
 @Composable
-fun TextTODOField(modifier: Modifier = Modifier){
+fun TextTODOField(modifier: Modifier = Modifier, value: TextFieldValue, onValueChange: (TextFieldValue) -> Unit,){
     TextField(
-        state = rememberTextFieldState(initialText = ""),
+        value = value,
+        onValueChange = onValueChange,
         placeholder = {Text(text = "Write TODO")},
         modifier = modifier
     )
 }
+
+data class Task(
+    val id:Int,
+    val title: String
+)
+
+@Composable
+fun TaskRow(tasks: Task){
+    Text(
+        text = "Task: ${tasks.title}",
+        modifier = Modifier.padding(8.dp)
+    )
+}
+
+@Composable
+fun TaskList(tasks: List<Task>){
+    LazyColumn {
+        items(tasks){
+            task -> TaskRow(task)
+        }
+    }
+}
+
+
